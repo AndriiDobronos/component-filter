43289ba8ddc7d46228280db7f5b4f2501b9c0f3a
Andrii Dobronos <andriidobronos@gmail.com>
2023-10-18 15:59:24 +0300

    changed photo

--- src/utils/allData.js
+++ src/utils/allData.js
@@ -1,20 +1,20 @@
-import Titenko from  "../static/photo/Titenko.jpg";
-import Maksimenko1 from "../static/photo/Maksimenko1.jpg";
-import Achasov1 from "../static/photo/Achasov1.jpg";
-import Baluk1 from "../static/photo/Baluk1.jpg";
-import Nekos1 from "../static/photo/Nekos1.jpg";
-import Sonko1 from "../static/photo/Sonko1.jpg";
-import Kalev from "../static/photo/Kalev.jpg";
-import Ruben from "../static/photo/Ruben.jpg";
-import Shpakivska1 from "../static/photo/Shpakivska1.jpg";
-import Shumilova1 from "../static/photo/Shumilova1.jpg";
-import Koval1 from "../static/photo/Koval1.jpg";
-import Gololobova1 from "../static/photo/Gololobova1.jpg";
-import Anton from "../static/photo/Anton.jpg";
-import Klesch1 from "../static/photo/Klesch1.jpg";
-import Bezrodnova1 from "../static/photo/Bezrodnova1.jpg";
-import Burchenko1 from "../static/photo/Burchenko1.jpg";
-import Grechko1 from "../static/photo/Grechko1.jpg";
+import alternate_email from  "../static/photo/alternate_email.svg";
+import background_drops from "../static/photo/background-drops.jpg";
+import contact_phone from "../static/photo/contact_phone.svg";
+import cv from "../static/photo/cv.png";
+import cv1 from "../static/photo/cv(1).png";
+import cv2 from "../static/photo/cv(2).png";
+import cv_products from "../static/photo/cv-products.svg";
+import dark_grey_stone from "../static/photo/dark-grey-stone.jpg";
+import deezer from "../static/photo/deezer.png";
+import facebook from "../static/photo/facebook.png";
+import github from "../static/photo/github.png";
+import github2 from "../static/photo/github(2).png";
+import global_network from "../static/photo/global-network.png";
+import globe from "../static/photo/globe.png";
+import gmail from "../static/photo/gmail.png";
+import gmail2 from "../static/photo/gmail(2).png";
+import google_maps from "../static/photo/google-maps.png";
 
 export default {
     navigation:[
@@ -30,42 +30,39 @@ export default {
         {id:"109",text:""},
     ],
     list:[
-        {id: "1", src: Titenko , alt: "Person photo",
-            post:"Голова наукового та організаційного комітету:",
-            name: " ТІТЕНКО Ганна Валеріївна", text: "директор ННІ екології, кандидат географічних наук, доцент.",sex: "female",size: true},
-        {id: "2", src: Maksimenko1, alt: "Person photo",
-            post:"Заступник голови наукового та організаційного комітету:",
-            name: "МАКСИМЕНКО Надія Василівна", text: "завідувач кафедри екологічного моніторингу та \n" +
-                "заповідної справи, доктор географічних наук, професор.",sex: "female",size: true,color:"white"},
-        {id: "3", src: Achasov1, alt: "Person photo",
-            name: "АЧАСОВ Андрій Борисович", text: "ННІ Екології, доктор с.-г. наук, професор.",sex:"male"},
-        {id: "4", src: Baluk1, alt: "Person photo",
-            name: "БАЛЮК Святослав Антонович", text: "ННЦ «Інститут грунтознавства і агрохімії імені О. Н. Соколовського», директор, академік НААН, доктор с.-г. наук, професор.",sex:"male"},
-        {id: "5", src: Nekos1, alt: "Person photo",
-            name: "НЕКОС Алла Наумівна", text: "ННІ Екології, доктор географічних наук, професор.",sex: "female",size: true},
-        {id: "6", src: Sonko1, alt: "Person photo",
-            name: "СОНЬКО Сергій Петрович", text: "Уманський національний університет садівництва, доктор географічних наук, професор.",sex: "male"},
-        {id: "7", src: Kalev, alt: "Person photo",
-            name: "КАЛЕВ СЕПП", text: "Доктор наук, професор. Естонський університет природничих наук, Тарту",sex:"male",color:"green"},
-        {id: "8", src: Ruben, alt: "Person photo",
-            name: "ХИНЕК РУБІК", text: "Доктор філософії, професор. Чеський університет природничих наук у Празі. Чеська республіка",sex: "male",color:"yellow"},
-        {id: "9", src: Shpakivska1, alt: "Person photo",
-            name: "ШПАКІВСЬКА Ірина Миронвна", text: "Інститут екології Карпат АН України, старший науковий співробітник, кандидат біологічних наук.",sex:"male"},
-        {id: "10", src: Shumilova1, alt: "Person photo",
-            name: "ШУМІЛОВА Алла Вікторівна", text: "НПП Слобожанський, кандидат географічних наук, начальник відділу.",sex: "female"},
-        {id: "11", src: Koval1, alt: "Person photo",
-            name: "КОВАЛЬ Ірина Михайлівна", text: "ННІ Екології, член-кореспондент Лісівничої академії наук України, доктор сільськогосподарських наук, старший науковий співробітник Укр.НДІЛГА ім. Г.М. Висоцького",sex: "female"},
-        {id: "12", src: Gololobova1, alt: "Person photo",
-            name: "ГОЛОЛОБОВА Олена Олександрівна", text: "ННІ Екології, кандидат сільськогосподарських наук, доцент.",sex: "female",size: true},
-        {id: "13", src: Anton, alt: "Person photo",
-            name: "ШКАРУБА Антон", text: "Кандидат географічних наук, старший науковий співробітник, Естонський університет природничих наук, Тарту",sex:"male",color:"red"},
-        {id: "14", src: Klesch1, alt: "Person photo",
-            name: "КЛЄЩ Анастасія Анатоліївна", text: "ННІ Екології, кандидат географічних наук, доцент.",sex: "female",size: true},
-        {id: "15", src: Bezrodnova1, alt: "Person photo",
-            name: "БЕЗРОДНОВА Ольга Володимирівна", text: "Біологічний факультет, кандидат біологічних наук, доцент.",sex: "female"},
-        {id: "16", src: Burchenko1, alt: "Person photo",
-            name: "БУРЧЕНКО Світлана Володимирівна", text: "ННІ Екології, старший викладач.",sex: "female"},
-        {id: "17", src: Grechko1, alt: "Person photo",
-            name:"ГРЕЧКО Аліна Андріївна", text: "Технічний секретар:",sex: "female"},
+        {id: "1", src: alternate_email, alt: "alternate_email",background:"transparent",
+            name: "alternate_email", text: "alternate_email",type:"svg",size:"48*48 bpp"},
+        {id: "2", src: background_drops, alt: "background_drops",type:"jpg", background:"transparent",
+            name: "background_drops", text: "background_drops",size: "4480*6720 bpp",color:"white"},
+        {id: "3", src: contact_phone, alt: "contact_phone",background:"transparent",
+            name: "contact_phone", text: "contact_phone",type:"svg",size:"48*48 bpp"},
+        {id: "4", src: cv, alt: "cv",background:"transparent",
+            name: "cv", text: "cv",type:"png",size:"512*512 bpp"},
+        {id: "5", src: cv1, alt: "cv1",background:"transparent",
+            name: "cv1", text: "cv1",type:"png",size:"512*512 bpp"},
+        {id: "6", src: cv2, alt: "cv2",background:"transparent",
+            name: "cv2", text: "cv2",type:"png",size:"512*512 bpp"},
+        {id: "7", src: cv_products, alt: "cv_products",background:"transparent",
+            name: "cv-products", text: "cv_products",type:"svg",size:"2500*2500 bpp"},
+        {id: "8", src: dark_grey_stone, alt: "dark_grey_stone",background:"transparent",
+            name: "dark_grey_stone", text: "dark_grey_stone",type:"jpg",size:"3024*4032 bpp"},
+        {id: "9", src: deezer, alt: "deezer",background:"transparent",
+            name: "deezer", text: "deezer",type:"png",size:"512*512 bpp"},
+        {id: "10", src: facebook, alt: "facebook",background:"transparent",
+            name: "facebook", text: "facebook",},
+        {id: "11", src: github, alt: "github",background:"transparent",
+            name: "github", text: "github",},
+        {id: "12", src: github2, alt: "github2",background:"transparent",
+            name: "github2", text: "github2",size: ""},
+        {id: "13", src: global_network, alt: "global_network",background:"transparent",
+            name: "global_network", text: "global_network"},
+        {id: "14", src: globe, alt: "globe",background:"transparent",
+            name: "globe", text: "globe",},
+        {id: "15", src: gmail, alt: "gmail",background:"transparent",
+            name: "gmail", text: "gmail",},
+        {id: "16", src: gmail2, alt: "gmail2",background:"transparent",
+            name: "gmail2", text: "gmail2",},
+        {id: "17", src: google_maps, alt: "google_maps",background:"transparent",
+            name:"google_maps", text: "google_maps",},
     ],
 }
